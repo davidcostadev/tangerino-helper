80f0b270bc5bc92d778d07f9a29fdae00cee1c8d
Eduardo Marinho <e@calmaamigo.com>
2018-11-02 14:57:22 -0300

    Use selector that doesn't change; wait for navigation

--- actions/login.js
+++ actions/login.js
@@ -9,14 +9,17 @@ const data = {
 
 async function doLogin (page) {
   console.log('Action'.bgWhite.black, 'doLogin');
-  
+
   await page.screenshot({path: './prints/01-before-login.png'});
   await page.click('.abaLogin li:nth-child(2) a');
   await page.screenshot({path: './prints/02-before-login.png'});
   await page.type('input[name="codigoEmpregador"]', data.codigoEmpregador);
   await page.type('input[name="pin"]', data.pin);
   await page.screenshot({path: './prints/03-before-login.png'});
-  await page.click('#idc');
+  await Promise.all([
+    page.click('form div:nth-child(6) input'),
+    page.waitForNavigation()
+  ]);
   await page.screenshot({path: './prints/04-after-login.png'});
 }
 
